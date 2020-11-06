1cdca97e44c6e8213202390c41c6b3bba82d54f5
caiseyann <caiseyann4@gmail.com>
2020-11-06 18:52:25 +0300

    added urls in online.urls.py

--- online/urls.py
+++ online/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'',include('store.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/accounts/login/'}),
+    url(r'^api-token-auth/', obtain_auth_token)
+  
 ]
