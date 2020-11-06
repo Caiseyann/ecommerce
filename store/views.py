03fdc79e31c6102697d93b93e586b5183a74f663
caiseyann <caiseyann4@gmail.com>
2020-11-06 18:38:33 +0300

    class profile views created

--- store/views.py
+++ store/views.py
@@ -13,3 +13,28 @@ def home(request):
     products = Project.get_products()
     profile = Profile.get_profile()
     return render(request, 'products/products.html', {"products": products})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,profile_id):
+
+    profile = Profile.objects.get(pk = profile_id)
+    projects = Project.objects.filter(profile_id=profile).all()
+
+    return render(request,"profile.html",{"profile":profile})
+
+
+@login_required(login_url='/accounts/login/')
+def add_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('home')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'new_profile.html', {"form": form})
+
