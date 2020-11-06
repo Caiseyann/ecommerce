70cda7666007175efc349ee0055ac4b2baad098d
caiseyann <caiseyann4@gmail.com>
2020-11-06 18:33:49 +0300

    class Profile created will hold all the information of a user's profile

--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo = models.ImageField(upload_to = 'profile/', null=True)
+    email = models.CharField(max_length=60, null=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
+    @property
+    def owner(self):
+        return self.user
+
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+        post_save.connect(create_user_profile, sender=User)
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+
+        return profile
+
