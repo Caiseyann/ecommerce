62fc9ad993a0775e9acc89dfc9bdcff4cfc381fc
caiseyann <caiseyann4@gmail.com>
2020-11-07 07:09:12 +0300

    updated urls includind sign in url

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static 
 from rest_framework.authtoken.views import obtain_auth_token
 
+
+
 urlpatterns = [
+    url(r'^registerPage/$', views.signUp, name='signPage'),
     url(r'^$', views.home,name='home'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^all/(?P<pk>\d+)', views.all, name='all'),
@@ -15,8 +19,6 @@ urlpatterns = [
     url(r'^api/products/$', views.ProductList.as_view()),
     url(r'api/products/products-id/(?P<pk>[0-9]+)/$',views.ProductsDescription.as_view()),
     url(r'api/products/products-id/delete(?P<pk>[0-9]+)/$',views.ProductsDescription.as_view()),
-
 ]
-
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
